e65672c8109068611192bebbeb084d83d14a2c1d
SuperSonic <supersonic@livemail.tw>
2022-04-02 18:26:20 +0800

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16
+FROM node:16-alpine
 
 # Create app directory
 WORKDIR /usr/src/app
@@ -15,5 +15,5 @@ RUN npm install
 # Bundle app source
 COPY . .
 
-EXPOSE 8080
-CMD [ "node", "server.js" ]
+EXPOSE 3000
+CMD [ "npm", "start" ]
--- app.js
+++ app.js
@@ -2,58 +2,39 @@
 
 require('dotenv').config();
 
-const APP_NAME = 'sara.recv';
-const SECRET_FILENAME = 'secret.key';
+const
+    crypto = require('crypto'),
+    http_status = require('http-status-codes'),
+    email_validator = require('email-validator');
 
-const fs = require('fs')
-const crypto = require('crypto');
-const express = require('express');
-const http_status = require('http-status-codes');
-const email_validator = require('email-validator');
+const user_schema = require("./src/schemas/user");
 
-const emailHandler = require('./src/mail');
-const tokenHandler = require('./src/token');
-
-const ctx = {}
-
-// JWT Security Check
-try {
-    ctx.jwt_secret = fs.readFileSync(SECRET_FILENAME).toString();
-} catch (e) {
-    if (e.code === 'ENOENT') {
-        throw 'JWT secret is unset, please generate one with "npm run new-secret"'
-    } else {
-        console.log(e)
+const
+    app = require('./src/init/express'),
+    constant = require('./src/init/const'),
+    ctx = {
+        database: require('./src/init/database'),
+        jwt_secret: require('./src/init/security')
+    },
+    util = {
+        email: require('./src/utils/mail'),
+        token: require('./src/utils/token')
     }
-}
-if (ctx.jwt_secret.length < 2048) {
-    throw 'JWT secret IS NOT SAFE, please generate the new one with "npm run new-secret"';
-}
-// Check end
-
-ctx.database = require('mongoose')
-void (ctx.database.connect(process.env.MONGODB_URI));
-
-const app = express();
-
-app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.send(APP_NAME)
+    res.send(constant.APP_NAME)
 });
 
 app.post('/login', async (req, res) => {
     if (!("email" in req.body)) {
         res.status(http_status.BAD_REQUEST).end();
     } else if (email_validator.validate(req.body.email)) {
-        const code = crypto.randomInt(999999);
-        emailHandler('login', {
-            website: process.env.WEBSITE_URL,
-            to: req.body.email,
-            ip_address: req.ip,
-            code
-        }).catch(console.error);
-        const next_token = await tokenHandler.issueNextToken(ctx, code, req.body.email);
+        const code = crypto.randomInt(100000, 999999);
+        const data = {website: process.env.WEBSITE_URL, to: req.body.email, ip_address: req.ip, code};
+        util.email('login', data).catch(console.error);
+        const User = ctx.database.model('User', user_schema);
+        if (!(await User.findOne({email: req.body.email}).exec())) return null;
+        const next_token = await util.token.issueCodeToken(ctx, code, {email: req.body.email});
         res.send({next_token});
     } else {
         res.status(http_status.FORBIDDEN).end();
@@ -65,27 +46,52 @@ app.post('/login/verify', async (req, res) => {
         res.status(http_status.BAD_REQUEST).end();
         return;
     }
-    const next_token = tokenHandler.validateNextToken(ctx, req.body.code, req.body.next_token);
+    const next_token = util.token.validateCodeToken(ctx, req.body.code, req.body.next_token);
     if (!next_token) {
         res.status(http_status.FORBIDDEN).end();
         return;
     }
-    const token = await tokenHandler.issueAuthToken(ctx, next_token.email);
+    const User = ctx.database.model('User', user_schema);
+    if (!(await User.findOne({email: req.body.email}).exec())) return null;
+    const token = await util.token.issueAuthToken(ctx, {email: req.body.email});
     res.send({token});
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     if (!("email" in req.body)) {
         res.status(http_status.BAD_REQUEST).end();
     } else if (email_validator.validate(req.body.email)) {
-        res.status(http_status.CREATED).end();
+        const code = crypto.randomInt(1000000, 9999999);
+        const data = {website: process.env.WEBSITE_URL, to: req.body.email, ip_address: req.ip, code};
+        util.email('register', data).catch(console.error);
+        const User = ctx.database.model('User', user_schema);
+        if (await User.findOne({email: req.body.email}).exec()) return null;
+        const register_token = await util.token.issueCodeToken(ctx, code, {email: req.body.email});
+        res.send({register_token});
     } else {
         res.status(http_status.FORBIDDEN).end();
     }
 });
 
+app.post('/register/verify', async (req, res) => {
+    if (("code" in req.body) && !("register_token" in req.body)) {
+        res.status(http_status.BAD_REQUEST).end();
+        return;
+    }
+    const register_token = util.token.validateCodeToken(ctx, req.body.code, req.body.register_token);
+    if (!register_token) {
+        res.status(http_status.FORBIDDEN).end();
+        return;
+    }
+    const User = ctx.database.model('User', user_schema);
+    if (await User.findOne({email: register_token.email}).exec()) return null;
+    const user = await (new User(register_token)).save();
+    const token = await util.token.issueAuthToken(ctx, user);
+    res.send({token});
+})
+
 app.listen(process.env.HTTP_PORT, () => {
-    console.log(APP_NAME)
+    console.log(constant.APP_NAME)
     console.log('====')
     console.log('Application is listening at')
     console.log(`http://localhost:${process.env.HTTP_PORT}`)
