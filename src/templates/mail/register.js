b090159e969a12a67024b94c5f2e39b7dc9fda07
SuperSonic <supersonic@livemail.tw>
2022-04-03 12:46:30 +0800

    Fix typos

--- src/templates/mail/register.js
+++ src/templates/mail/register.js
@@ -9,13 +9,13 @@ module.exports = {
         
         若您未曾請求過該代碼，請您無視本電子郵件。
         
-        「Sara 系統」是一個開放原始碼的無密碼式身份認證解決方案，由臺灣網際網路技術推廣組織（https://web-tech-tw.github.io）提供技術支援。
+        「Sara系統」是一個開放原始碼的無密碼式身份認證解決方案，由臺灣網際網路技術推廣組織（https://web-tech-tw.github.io）提供技術支援。
     `,
     html: (data) => `
         您好，這裡是 ${data.website} 網站，有人申請使用您的信箱 ${data.to} 進行註冊<br/>
         <p>這裡是您的註冊代號：<br/><code>${data.code}</code></p>
         <p>若您未曾請求過該代碼，請您無視本電子郵件。</p>
-        「Sara 系統」是一個開放原始碼的無密碼式身份認證解決方案，由
+        「Sara系統」是一個開放原始碼的無密碼式身份認證解決方案，由
         <a href="https://web-tech-tw.github.io">臺灣網際網路技術推廣組織</a>
         提供技術支援。<br/>
     `,
