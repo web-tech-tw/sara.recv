fa29c76711fe1f99fc432e4c9835104f050903f5
Shiranui <supersonic@livemail.tw>
2023-05-08 15:29:58 +0800

    refactor: update for docs

--- src/init/const.js
+++ src/init/const.js
@@ -2,6 +2,13 @@
 // Constants
 
 exports.APP_NAME = "sara.recv";
+exports.APP_DESCRIPTION = "無密碼式身分認證系統。A passwordless authentication system.";
+
+exports.APP_VERSION = "latest";
+
+exports.APP_AUTHOR_NAME = "Taiwan Web Technology Promotion Organization";
+exports.APP_AUTHOR_URL = "https://web-tech-tw.github.io";
+
 exports.SECRET_FILENAME = "secret.key";
 
 exports.TEST_EMAIL_DOMAIN = "web-tech-tw.github.io";
--- src/routes/swagger.js
+++ src/routes/swagger.js
@@ -1,30 +1,39 @@
 "use strict";
 
-const {getEnabled} = require("../config");
-const {APP_NAME} = require("../init/const");
+const {getEnabled, getMust} = require("../config");
+
+const {
+    APP_NAME,
+    APP_DESCRIPTION,
+    APP_VERSION,
+    APP_AUTHOR_NAME,
+    APP_AUTHOR_URL,
+} = require("../init/const");
 
 const {useApp, express} = require("../init/express");
 const {join: pathJoin} = require("path");
 
 const {routerFiles} = require("./index");
-const {modelFiles} = require("../models");
 
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-const toDocSchema = require("mongoose-to-swagger");
 
 const options = {
     definition: {
         openapi: "3.0.0",
         info: {
             title: APP_NAME,
-            version: "latest",
-            description: "無密碼式身分認證系統。A passwordless authentication system.",
+            version: APP_VERSION,
+            description: APP_DESCRIPTION,
             contact: {
-                name: "Taiwan Web Technology Promotion Organization",
-                url: "https://web-tech-tw.github.io",
+                name: APP_AUTHOR_NAME,
+                url: APP_AUTHOR_URL,
             },
         },
+        servers: [{
+            description: getMust("SWAGGER_SERVER_DESCRIPTION"),
+            url: getMust("SWAGGER_SERVER_URL"),
+        }],
         components: {
             securitySchemes: {
                 ApiKeyAuth: {
@@ -33,14 +42,6 @@ const options = {
                     name: "Authorization",
                 },
             },
-            schemas: Object.fromEntries(modelFiles.map(
-                (f) => {
-                    const filename = pathJoin(__dirname, "../models", f);
-                    const model = require(filename);
-                    const schema = toDocSchema(model);
-                    return [schema.title, schema];
-                },
-            )),
         },
     },
     apis: routerFiles.map(
