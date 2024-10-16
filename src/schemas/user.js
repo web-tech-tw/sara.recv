9ba57f0331e176e4a74f902c60dd740b40e4f5c3
不知火 Shiranui <supersonic@livemail.tw>
2024-10-16 16:56:04 +0000

    fix(user.js): security update with schema

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -4,9 +4,25 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 module.exports = new Schema({
-    email: String,
-    nickname: String,
-    roles: [String],
-    created_at: Number,
-    updated_at: Number,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    nickname: {
+        type: String,
+        required: true,
+    },
+    roles: {
+        type: [String],
+        default: [],
+    },
+    created_at: {
+        type: Number,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Number,
+        default: Date.now,
+    },
 });
